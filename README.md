e0aae2f658c8e7eeaffb3b206c4159c17e1786cc
Rickard Sandvik <mrsandvik@gmail.com>
2023-02-09 11:13:57 +0100

    first update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NavneListe
 Navnegenerator
+Her kan dere lage dere noen navn og se litt på memes.
+Det kan også lages tilfeldige bilder hvis det er behov for det
--- java.js
+++ java.js
@@ -1,9 +1,11 @@
 const generateButton1 = document.getElementById("generateButton1");
 const generateButton2 = document.getElementById("generateButton2");
 const generateButton3 = document.getElementById("generateButton3");
-const generateButton4 = document.getElementById("generateButton4");
+const generateButton4 = document.getElementById("generateMemeButton");
 const namesElement = document.getElementById("names");
 const memeElement = document.getElementById("meme");
+
+//Buttons
 
 generateButton1.addEventListener("click", function() {
   fetchData(1);
@@ -20,6 +22,8 @@ generateButton3.addEventListener("click", function() {
 generateButton4.addEventListener("click", function() {
   fetchMeme();
 });
+
+//Random Name API Call
 
 function fetchData(numberOfNames) {
   fetch(`https://sheets.googleapis.com/v4/spreadsheets/1b5Uf6JxBc-F1yazI_653cYkR2pxTFCyU2qnUkfIuo9o/values/Sheet1!A1:D?key=AIzaSyB1bQ9kyYXALIHZLOir7rHeSxzENpOWDTU`)
@@ -38,9 +42,59 @@ function fetchData(numberOfNames) {
     });
 }
 
+//Random meme API call
+
 async function fetchMeme() {
-  const response = await fetch('https://meme-api.herokuapp.com/gimme');
-  const data = await response.json();
-  const randomMeme = data.url;
-  memeElement.innerHTML = `<img src="${randomMeme}" alt="random meme">`;
+  fetch("https://trending-memes-9gag.p.rapidapi.com/v1/fetch-sections?offset=0&limit=20&filter=Animal", {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "e78973b520mshaeeaad81a2125e3p1599c0jsna5fb4b93eaac",
+      "X-RapidAPI-Host": "trending-memes-9gag.p.rapidapi.com"
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (!data.data) {
+        throw new Error("No data received");
+      }
+      const randomIndex = Math.floor(Math.random() * data.data.length);
+      const randomMeme = data.data[randomIndex];
+      document.getElementById("memeImage").src = randomMeme.images.large.url;
+    })
+    .catch(error => console.error("There was a problem with the fetch operation: ", error));
 }
+
+document.getElementById("generateMemeButton").addEventListener("click", fetchMeme);
+
+//Random Image 
+const button = document.getElementById("fetchButton");
+button.addEventListener("click", function() {
+  var category = 'nature'
+  fetch(`https://api.api-ninjas.com/v1/randomimage?category=${category}`, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": "YOUR_API_KEY",
+      "Accept": "image/jpg"
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`There was a problem with the fetch operation: ${response.statusText}`);
+      }
+      return response.blob();
+    })
+    .then(imageBlob => {
+      const imageUrl = URL.createObjectURL(imageBlob);
+      console.log(imageUrl);
+    })
+    .catch(error => {
+      console.error(error.message);
+    });
+});
+
+
